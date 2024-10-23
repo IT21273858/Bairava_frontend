242f058e613a7c14e74a0d2e0d1e7a7195c396c6
IT21273858 <it21273858@my.sliit.lk>
2024-10-23 23:16:21 +0530

    build url implemented

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,11 @@ config();
 export default defineConfig({
   define: {
     'process.env': process.env,
-    'process.env.VITE_REACT_APP_BASE_URL': JSON.stringify('http://localhost:8000'),
-    'import.meta.env.VITE_REACT_APP_BASE_URL': JSON.stringify('http://localhost:8000'),
+    // 'process.env.VITE_REACT_APP_BASE_URL': JSON.stringify('http://localhost:8000'),
+    // 'import.meta.env.VITE_REACT_APP_BASE_URL': JSON.stringify('http://localhost:8000'),
     // 'process.env.VITE_REACT_APP_BASE_URL': JSON.stringify('https://vl-backend-615957435766.asia-southeast1.run.app'),
     // 'import.meta.env.VITE_REACT_APP_BASE_URL': JSON.stringify('https://vl-backend-615957435766.asia-southeast1.run.app'),
+    'import.meta.env.VITE_REACT_APP_BASE_URL': JSON.stringify('https://9vhcpx4m-8000.asse.devtunnels.ms'),
   },
   plugins: [react()],
   css: {
